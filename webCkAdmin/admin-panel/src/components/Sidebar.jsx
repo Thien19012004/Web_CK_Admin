a7bd5d6964979f88f3d5ad4e9c69f8e894af3f59
Thien19012004 <168295834+Thien19012004@users.noreply.github.com>
2024-12-21 01:38:58 +0700

    Add: basic front - end
    Admin_v1

---
+++ webCkAdmin/admin-panel/src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <div style={{ width: "200px", background: "#333", color: "#fff", height: "100vh" }}>
+      <h2>Admin Panel</h2>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/" style={{ color: "#fff", textDecoration: "none" }}>Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/products" style={{ color: "#fff", textDecoration: "none" }}>Products</Link>
+          </li>
+          <li>
+            <Link to="/users" style={{ color: "#fff", textDecoration: "none" }}>Users</Link>
+          </li>
+          <li>
+            <Link to="/orders" style={{ color: "#fff", textDecoration: "none" }}>Orders</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Sidebar;
