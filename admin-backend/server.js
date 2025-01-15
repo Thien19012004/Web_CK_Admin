823b387e709492ed88dabedb7ef7df53945f4afd
Thien19012004 <168295834+Thien19012004@users.noreply.github.com>
2025-01-15 18:14:55 +0700

    Update server.js

--- admin-backend/server.js
+++ admin-backend/server.js
@@ -13,7 +13,11 @@ const app = express();
 
 // Middleware
 // Cấu hình CORS
-app.use(cors());
+app.use(cors({
+    origin: 'https://admin.lptdevops.website', // Hoặc '*'
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use(express.json());
 
